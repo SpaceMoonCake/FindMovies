8dd2e8e9bb352553b77a46330e54e906981dec05
moonCake <klamina259@gmail.com>
2022-06-08 20:36:37 +0300

    create details film fragment

--- app/src/main/java/com/spacemooncake/findmovies/model/entities/Film.kt
+++ app/src/main/java/com/spacemooncake/findmovies/model/entities/Film.kt
@@ -1,8 +1,12 @@
 package com.spacemooncake.findmovies.model.entities
 
+import android.os.Parcelable
 import com.spacemooncake.findmovies.R
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
+
+@Parcelize
 data class Film(
     val filmId: String = "12sf32467",
     val filmName: String = "Невероятный фильм",
@@ -10,4 +14,4 @@ data class Film(
     var filmRating: Double = 4.3,
     val filmDate: String = "21.10.2020",
     var filmPoster: Int = R.drawable.poster_example2
-)
+) : Parcelable
